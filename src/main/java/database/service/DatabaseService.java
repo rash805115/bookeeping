620553ea58bcefcf892e1269996fe154ad236397
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-02-28 12:26:27 -0500

    Methods Added
    Two new methods to set and get a user is added.
    Set - to create a new user.
    Get - to get a valid existing user.

--- src/main/java/database/service/DatabaseService.java
+++ src/main/java/database/service/DatabaseService.java
@@ -1,6 +1,13 @@
 package database.service;
 
+import java.util.Map;
+
+import exception.DuplicateUser;
+import exception.UserNotFound;
+
 public interface DatabaseService
 {
 	public int getNextAutoIncrement();
+	public void createNewUser(String userId, Map<String, Object> userProperties) throws DuplicateUser;
+	public Map<String, Object> getUser(String userId) throws UserNotFound;
 }

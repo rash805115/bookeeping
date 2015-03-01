1d7cb4d63bd1dfcc650b143b691114e2d526c258
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-02-28 23:09:14 -0500

    TitanEmbedded set & get user success
    The titan embedded successfully implemented the set and get user methods
    defined by databaseService.
    
    The test file was updated to have the following:
    1. Junit 4
    2. BeforeClass and AfterClass functions for one time setup.
    3. Test case updates to include all test cases in the implementation
    class.
    4.Impl class now has a better transaction handling.
    
    This commit also fixes issue #3.
    Index was not getting recogized because the propertyName under property
    key was wrong. Corrected it to show nodeId instead of nodeIdPropertyKey
    and userId instead of userIdPropertyKey.

--- src/main/java/database/service/impl/TitanEmbeddedServiceImpl.java
+++ src/main/java/database/service/impl/TitanEmbeddedServiceImpl.java
@@ -1,19 +1,24 @@
 package database.service.impl;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.configuration.BaseConfiguration;
 
 import utilities.configurationproperties.DatabaseConnectionProperty;
 
 import com.thinkaurelius.titan.core.PropertyKey;
-import com.thinkaurelius.titan.core.TitanException;
 import com.thinkaurelius.titan.core.TitanFactory;
 import com.thinkaurelius.titan.core.TitanGraph;
+import com.thinkaurelius.titan.core.TitanTransaction;
 import com.thinkaurelius.titan.core.schema.TitanManagement;
 import com.tinkerpop.blueprints.Vertex;
 
 import database.service.DatabaseService;
+import exception.DuplicateUser;
+import exception.UserNotFound;
 
 public class TitanEmbeddedServiceImpl implements DatabaseService
 {
@@ -44,67 +49,155 @@ public class TitanEmbeddedServiceImpl implements DatabaseService
 				titanManagement.makeVertexLabel("AutoIncrement").make();
 			}
 			
-			if(! this.titanGraph.containsPropertyKey("nodeIdPropertyKey"))
+			if(! this.titanGraph.containsVertexLabel("User"))
 			{
-				PropertyKey nodeIdPropertyKey = titanManagement.makePropertyKey("nodeIdPropertyKey").dataType(Long.class).make();
+				titanManagement.makeVertexLabel("User").make();
+			}
+			
+			if(! this.titanGraph.containsPropertyKey("nodeId"))
+			{
+				PropertyKey nodeIdPropertyKey = titanManagement.makePropertyKey("nodeId").dataType(Integer.class).make();
 				titanManagement.buildIndex("nodeIdIndex", Vertex.class).addKey(nodeIdPropertyKey).unique().buildCompositeIndex();
 			}
+			
+			if(! this.titanGraph.containsPropertyKey("userId"))
+			{
+				PropertyKey userIdPropertyKey = titanManagement.makePropertyKey("userId").dataType(String.class).make();
+				titanManagement.buildIndex("userIdIndex", Vertex.class).addKey(userIdPropertyKey).unique().buildCompositeIndex();
+			}
+			
+			titanManagement.commit();
 		}
-		catch(TitanException titanException)
+		finally
 		{
-			System.out.println("ERROR: Unable to setup titan graph.");
-			titanException.printStackTrace();
-			System.exit(1);
+			if(titanManagement.isOpen())
+			{
+				titanManagement.rollback();
+			}
 		}
-		
-		titanManagement.commit();
 	}
 
 	@Override
 	public int getNextAutoIncrement()
 	{
-		Iterator<Vertex> iterator = this.titanGraph.getVertices("nodeId", 0).iterator();
-		Vertex autoIncrement = null;
-		while(iterator.hasNext())
-		{
-			autoIncrement = iterator.next();
-		}
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
 		
-		if(autoIncrement == null)
+		try
 		{
-			try
+			Iterator<Vertex> iterator = this.titanGraph.getVertices("nodeId", 0).iterator();
+			Vertex autoIncrement = null;
+			while(iterator.hasNext())
 			{
-				this.titanGraph.newTransaction();
-				
+				autoIncrement = iterator.next();
+			}
+			
+			if(autoIncrement == null)
+			{
 				Vertex vertex = this.titanGraph.addVertexWithLabel("AutoIncrement");
 				vertex.setProperty("nodeId", 0);
-				vertex.setProperty("next", 1);
+				vertex.setProperty("next", 2);
 				
-				this.titanGraph.commit();
-				return 0;
+				titanTransaction.commit();
+				return 1;
 			}
-			catch(TitanException titanException)
+			else
 			{
-				titanException.printStackTrace();
-				return -1;
-			}
-		}
-		else
-		{
-			try
-			{
-				this.titanGraph.newTransaction();
-				
 				int nextIncrement = autoIncrement.getProperty("next");
 				autoIncrement.setProperty("next", nextIncrement + 1);
 				
-				this.titanGraph.commit();
+				titanTransaction.commit();
 				return nextIncrement;
 			}
-			catch(TitanException titanException)
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
 			{
-				titanException.printStackTrace();
-				return -1;
+				titanTransaction.rollback();
+			}
+		}
+	}
+
+	@Override
+	public void createNewUser(String userId, Map<String, Object> userProperties) throws DuplicateUser
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			Iterator<Vertex> iterator = this.titanGraph.getVertices("userId", userId).iterator();
+			Vertex user = null;
+			while(iterator.hasNext())
+			{
+				user = iterator.next();
+			}
+			
+			if(user != null)
+			{
+				titanTransaction.commit();
+				throw new DuplicateUser("ERROR: User already present! - \"" + userId + "\"");
+			}
+			else
+			{
+				Vertex vertex = this.titanGraph.addVertexWithLabel("User");
+				vertex.setProperty("nodeId", this.getNextAutoIncrement());
+				vertex.setProperty("userId", userId);
+				
+				for(Entry<String, Object> userPropertiesEntry : userProperties.entrySet())
+				{
+					vertex.setProperty(userPropertiesEntry.getKey(), userPropertiesEntry.getValue());
+				}
+				
+				titanTransaction.commit();
+			}
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
+			}
+		}
+	}
+
+	@Override
+	public Map<String, Object> getUser(String userId) throws UserNotFound
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			Iterator<Vertex> iterator = this.titanGraph.getVertices("userId", userId).iterator();
+			Vertex user = null;
+			while(iterator.hasNext())
+			{
+				user = iterator.next();
+			}
+			
+			if(user == null)
+			{
+				titanTransaction.commit();
+				throw new UserNotFound("ERROR: User not found! - \"" + userId + "\"");
+			}
+			else
+			{
+				Map<String, Object> userProperties = new HashMap<String, Object>();
+				
+				Iterable<String> iterable = user.getPropertyKeys();
+				for(String key : iterable)
+				{
+					userProperties.put(key, user.getProperty(key));
+				}
+				
+				titanTransaction.commit();
+				return userProperties;
+			}
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
 			}
 		}
 	}
--- src/test/java/database/service/impl/TitanEmbeddedServiceImplTest.java
+++ src/test/java/database/service/impl/TitanEmbeddedServiceImplTest.java
@@ -1,19 +1,98 @@
 package database.service.impl;
 
-import database.service.DatabaseService;
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class TitanEmbeddedServiceImplTest extends TestCase
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import database.service.DatabaseService;
+import exception.DuplicateUser;
+import exception.UserNotFound;
+
+public class TitanEmbeddedServiceImplTest
 {
-	private DatabaseService databaseService;
+	private static DatabaseService databaseService;
 	
-	public TitanEmbeddedServiceImplTest()
+	@BeforeClass
+	public static void globalSetup()
 	{
-		this.databaseService = new TitanEmbeddedServiceImpl();
+		TitanEmbeddedServiceImplTest.databaseService = new TitanEmbeddedServiceImpl();
 	}
 	
+	@AfterClass
+	public static void globalTearDown()
+	{
+		TitanEmbeddedServiceImplTest.databaseService = null;
+	}
+	
+	@Test
 	public void testGetNextAutoIncrement()
 	{
-		assertTrue(this.databaseService.getNextAutoIncrement() >= 0);
+		long nextAutoIncrement1 = TitanEmbeddedServiceImplTest.databaseService.getNextAutoIncrement();
+		long nextAutoIncrement2 = TitanEmbeddedServiceImplTest.databaseService.getNextAutoIncrement();
+		assertEquals(nextAutoIncrement1 + 1, nextAutoIncrement2);
+	}
+	
+	@Test
+	public void testUserSetGet()
+	{
+		String userId = "testUser";
+		Map<String, Object> userProperties = new HashMap<String, Object>();
+		userProperties.put("firstName", "Test");
+		userProperties.put("lastName", "User");
+		userProperties.put("email", "testuser@test.com");
+		
+		try
+		{
+			TitanEmbeddedServiceImplTest.databaseService.getUser(userId);
+			assertFalse("Expected Exception UserNotFound. Got none!", true);
+		}
+		catch(UserNotFound userNotFound)
+		{
+			assertTrue(true);
+		}
+		
+		try
+		{
+			TitanEmbeddedServiceImplTest.databaseService.createNewUser(userId, userProperties);
+			assertTrue(true);
+		}
+		catch(DuplicateUser duplicateUser)
+		{
+			assertFalse(duplicateUser.getMessage(), true);
+		}
+		
+		try
+		{
+			TitanEmbeddedServiceImplTest.databaseService.createNewUser(userId, userProperties);
+			assertFalse("Expected Exception DuplicateUser. Got none!", true);
+		}
+		catch(DuplicateUser duplicateUser)
+		{
+			assertTrue(true);
+		}
+		
+		try
+		{
+			Map<String, Object> retrievedUserProperties = TitanEmbeddedServiceImplTest.databaseService.getUser(userId);
+			for(Entry<String, Object> entry : userProperties.entrySet())
+			{
+				String key = entry.getKey();
+				Object value = entry.getValue();
+				
+				assertTrue(value.equals(retrievedUserProperties.get(key)));
+			}
+		}
+		catch(UserNotFound userNotFound)
+		{
+			assertFalse(userNotFound.getMessage(), true);
+		}
 	}
 }

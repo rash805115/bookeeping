9b8a43361b484c84f26ce3fca8b62cbf7d20852c
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-02-28 19:49:23 -0500

    Neo4JRest set & get user success
    The neo4j rest successfully implemented the set and get user methods
    defined by databaseService.
    
    The test file was updated to have the following:
    1. Junit 4
    2. BeforeClass and AfterClass functions for one time setup.
    3. Test case updates to include all test cases in the implementation
    class.

--- src/main/java/database/service/impl/Neo4JRestServiceImpl.java
+++ src/main/java/database/service/impl/Neo4JRestServiceImpl.java
@@ -1,5 +1,9 @@
 package database.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
@@ -9,16 +13,15 @@ import org.neo4j.graphdb.index.ReadableIndex;
 import org.neo4j.rest.graphdb.RestGraphDatabase;
 
 import utilities.configurationproperties.DatabaseConnectionProperty;
-
-import com.thinkaurelius.titan.core.TitanException;
-
 import database.service.DatabaseService;
+import exception.DuplicateUser;
+import exception.UserNotFound;
 
 public class Neo4JRestServiceImpl implements DatabaseService
 {
 	private enum Labels implements Label
 	{
-		AutoIncrement
+		AutoIncrement, User
 	}
 	
 	private GraphDatabaseService graphDatabaseService;
@@ -38,15 +41,10 @@ public class Neo4JRestServiceImpl implements DatabaseService
 		{
 			AutoIndexer<Node> autoIndexer = this.graphDatabaseService.index().getNodeAutoIndexer();
 			autoIndexer.startAutoIndexingProperty("nodeId");
+			autoIndexer.startAutoIndexingProperty("userId");
 			autoIndexer.setEnabled(true);
 			
 			transaction.success();
-		}
-		catch(TitanException titanException)
-		{
-			System.out.println("ERROR: Unable to setup neo4j-rest graph.");
-			titanException.printStackTrace();
-			System.exit(1);
 		}
 	}
 
@@ -58,16 +56,8 @@ public class Neo4JRestServiceImpl implements DatabaseService
 		{
 			ReadableIndex<Node> readableIndex = this.graphDatabaseService.index().getNodeAutoIndexer().getAutoIndex();
 			autoIncrement = readableIndex.get("nodeId", 0).getSingle();
-		}
-		catch(Exception exception)
-		{
-			exception.printStackTrace();
-			return -1;
-		}
-		
-		if(autoIncrement == null)
-		{
-			try(Transaction transaction = this.graphDatabaseService.beginTx())
+			
+			if(autoIncrement == null)
 			{
 				Node node = this.graphDatabaseService.createNode(Labels.AutoIncrement);
 				node.setProperty("nodeId", 0);
@@ -76,15 +66,7 @@ public class Neo4JRestServiceImpl implements DatabaseService
 				transaction.success();
 				return 0;
 			}
-			catch(Exception exception)
-			{
-				exception.printStackTrace();
-				return -1;
-			}
-		}
-		else
-		{
-			try(Transaction transaction = this.graphDatabaseService.beginTx())
+			else
 			{
 				int nextAutoIncrement = (int) autoIncrement.getProperty("next");
 				autoIncrement.setProperty("next", nextAutoIncrement + 1);
@@ -92,10 +74,65 @@ public class Neo4JRestServiceImpl implements DatabaseService
 				transaction.success();
 				return nextAutoIncrement;
 			}
-			catch(Exception exception)
+		}
+	}
+
+	@Override
+	public void createNewUser(String userId, Map<String, Object> userProperties) throws DuplicateUser
+	{
+		Node user = null;
+		try(Transaction transaction = this.graphDatabaseService.beginTx())
+		{
+			ReadableIndex<Node> readableIndex = this.graphDatabaseService.index().getNodeAutoIndexer().getAutoIndex();
+			user = readableIndex.get("userId", userId).getSingle();
+			
+			if(user != null)
 			{
-				exception.printStackTrace();
-				return -1;
+				transaction.success();
+				throw new DuplicateUser("ERROR: User already present! - \"" + userId + "\"");
+			}
+			else
+			{
+				Node node = this.graphDatabaseService.createNode(Labels.User);
+				node.setProperty("nodeId", this.getNextAutoIncrement());
+				node.setProperty("userId", userId);
+				
+				for(Entry<String, Object> userPropertiesEntry : userProperties.entrySet())
+				{
+					node.setProperty(userPropertiesEntry.getKey(), userPropertiesEntry.getValue());
+				}
+				
+				transaction.success();
+			}
+		}
+	}
+
+	@Override
+	public Map<String, Object> getUser(String userId) throws UserNotFound
+	{
+		Node user = null;
+		try(Transaction transaction = this.graphDatabaseService.beginTx())
+		{
+			ReadableIndex<Node> readableIndex = this.graphDatabaseService.index().getNodeAutoIndexer().getAutoIndex();
+			user = readableIndex.get("userId", userId).getSingle();
+			
+			if(user == null)
+			{
+				transaction.success();
+				throw new UserNotFound("ERROR: User not found! - \"" + userId + "\"");
+			}
+			else
+			{
+				Map<String, Object> userProperties = new HashMap<String, Object>();
+				
+				Iterable<String> iterable = user.getPropertyKeys();
+				for(String key : iterable)
+				{
+					userProperties.put(key, user.getProperty(key));
+				}
+				
+				transaction.success();
+				return userProperties;
 			}
 		}
 	}
--- src/test/java/database/service/impl/Neo4JRestServiceImplTest.java
+++ src/test/java/database/service/impl/Neo4JRestServiceImplTest.java
@@ -1,21 +1,99 @@
 package database.service.impl;
 
-import database.service.DatabaseService;
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class Neo4JRestServiceImplTest extends TestCase
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import database.service.DatabaseService;
+import exception.DuplicateUser;
+import exception.UserNotFound;
+
+public class Neo4JRestServiceImplTest
 {
-	private DatabaseService databaseService;
+	private static DatabaseService databaseService;
 	
-	public Neo4JRestServiceImplTest()
+	@BeforeClass
+	public static void globalSetup()
 	{
-		this.databaseService = new Neo4JRestServiceImpl();
+		Neo4JRestServiceImplTest.databaseService = new Neo4JRestServiceImpl();
 	}
 	
+	@AfterClass
+	public static void globalTearDown()
+	{
+		Neo4JRestServiceImplTest.databaseService = null;
+	}
+	
+	@Test
 	public void testGetNextAutoIncrement()
 	{
-		long nextAutoIncrement1 = this.databaseService.getNextAutoIncrement();
-		long nextAutoIncrement2 = this.databaseService.getNextAutoIncrement();
+		long nextAutoIncrement1 = Neo4JRestServiceImplTest.databaseService.getNextAutoIncrement();
+		long nextAutoIncrement2 = Neo4JRestServiceImplTest.databaseService.getNextAutoIncrement();
+		
 		assertEquals(nextAutoIncrement1 + 1, nextAutoIncrement2);
+	}
+	
+	@Test
+	public void testUserSetGet()
+	{
+		String userId = "testUser";
+		Map<String, Object> userProperties = new HashMap<String, Object>();
+		userProperties.put("firstName", "Test");
+		userProperties.put("lastName", "User");
+		userProperties.put("email", "testuser@test.com");
+		
+		try
+		{
+			Neo4JRestServiceImplTest.databaseService.getUser(userId);
+			assertFalse("Expected Exception UserNotFound. Got none!", true);
+		}
+		catch(UserNotFound userNotFound)
+		{
+			assertTrue(true);
+		}
+		
+		try
+		{
+			Neo4JRestServiceImplTest.databaseService.createNewUser(userId, userProperties);
+			assertTrue(true);
+		}
+		catch(DuplicateUser duplicateUser)
+		{
+			assertFalse(duplicateUser.getMessage(), true);
+		}
+		
+		try
+		{
+			Neo4JRestServiceImplTest.databaseService.createNewUser(userId, userProperties);
+			assertFalse("Expected Exception DuplicateUser. Got none!", true);
+		}
+		catch(DuplicateUser duplicateUser)
+		{
+			assertTrue(true);
+		}
+		
+		try
+		{
+			Map<String, Object> retrievedUserProperties = Neo4JRestServiceImplTest.databaseService.getUser(userId);
+			for(Entry<String, Object> entry : userProperties.entrySet())
+			{
+				String key = entry.getKey();
+				Object value = entry.getValue();
+				
+				assertTrue(value.equals(retrievedUserProperties.get(key)));
+			}
+		}
+		catch(UserNotFound userNotFound)
+		{
+			assertFalse(userNotFound.getMessage(), true);
+		}
 	}
 }

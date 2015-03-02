20c23ebbd3dc709abedf1060e0db9a4626d2c8c9
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-03-02 18:46:25 -0500

    TitanEmbeddedServiceImpl modularized

---
+++ src/main/java/database/service/titancassandraembedded/impl/AutoIncrementServiceImpl.java
@@ -0,0 +1,62 @@
+package database.service.titancassandraembedded.impl;
+
+import java.util.Iterator;
+
+import com.thinkaurelius.titan.core.TitanGraph;
+import com.thinkaurelius.titan.core.TitanTransaction;
+import com.tinkerpop.blueprints.Vertex;
+
+import database.NodeLabels;
+import database.connection.singleton.TitanCassandraEmbeddedConnection;
+import database.service.AutoIncrementService;
+
+public class AutoIncrementServiceImpl implements AutoIncrementService
+{
+	private TitanGraph titanGraph;
+	
+	public AutoIncrementServiceImpl()
+	{
+		this.titanGraph = TitanCassandraEmbeddedConnection.getInstance().getTitanGraphObject();
+	}
+	
+	@Override
+	public int getNextAutoIncrement()
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			Iterator<Vertex> iterator = this.titanGraph.getVertices("nodeId", 0).iterator();
+			Vertex autoIncrement = null;
+			while(iterator.hasNext())
+			{
+				autoIncrement = iterator.next();
+			}
+			
+			if(autoIncrement == null)
+			{
+				Vertex vertex = this.titanGraph.addVertexWithLabel(NodeLabels.AutoIncrement.name());
+				vertex.setProperty("nodeId", 0);
+				vertex.setProperty("next", 2);
+				
+				titanTransaction.commit();
+				return 1;
+			}
+			else
+			{
+				int nextIncrement = autoIncrement.getProperty("next");
+				autoIncrement.setProperty("next", nextIncrement + 1);
+				
+				titanTransaction.commit();
+				return nextIncrement;
+			}
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
+			}
+		}
+	}
+}
--- src/main/java/database/service/titancassandraembedded/impl/UserServiceImpl.java
+++ src/main/java/database/service/titancassandraembedded/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package database.service.impl;
+package database.service.titancassandraembedded.impl;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -9,61 +9,21 @@ import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.TitanTransaction;
 import com.tinkerpop.blueprints.Vertex;
 
-import database.connection.singleton.TitanEmbeddedConnection;
-import database.service.DatabaseService;
+import database.NodeLabels;
+import database.connection.singleton.TitanCassandraEmbeddedConnection;
+import database.service.UserService;
 import exception.DuplicateUser;
 import exception.UserNotFound;
 
-public class TitanEmbeddedServiceImpl implements DatabaseService
+public class UserServiceImpl implements UserService
 {
 	private TitanGraph titanGraph;
 	
-	public TitanEmbeddedServiceImpl()
+	public UserServiceImpl()
 	{
-		this.titanGraph = TitanEmbeddedConnection.getInstance().getTitanGraphObject();
+		this.titanGraph = TitanCassandraEmbeddedConnection.getInstance().getTitanGraphObject();
 	}
-
-	@Override
-	public int getNextAutoIncrement()
-	{
-		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
-		
-		try
-		{
-			Iterator<Vertex> iterator = this.titanGraph.getVertices("nodeId", 0).iterator();
-			Vertex autoIncrement = null;
-			while(iterator.hasNext())
-			{
-				autoIncrement = iterator.next();
-			}
-			
-			if(autoIncrement == null)
-			{
-				Vertex vertex = this.titanGraph.addVertexWithLabel("AutoIncrement");
-				vertex.setProperty("nodeId", 0);
-				vertex.setProperty("next", 2);
-				
-				titanTransaction.commit();
-				return 1;
-			}
-			else
-			{
-				int nextIncrement = autoIncrement.getProperty("next");
-				autoIncrement.setProperty("next", nextIncrement + 1);
-				
-				titanTransaction.commit();
-				return nextIncrement;
-			}
-		}
-		finally
-		{
-			if(titanTransaction.isOpen())
-			{
-				titanTransaction.rollback();
-			}
-		}
-	}
-
+	
 	@Override
 	public void createNewUser(String userId, Map<String, Object> userProperties) throws DuplicateUser
 	{
@@ -85,8 +45,8 @@ public class TitanEmbeddedServiceImpl implements DatabaseService
 			}
 			else
 			{
-				Vertex vertex = this.titanGraph.addVertexWithLabel("User");
-				vertex.setProperty("nodeId", this.getNextAutoIncrement());
+				Vertex vertex = this.titanGraph.addVertexWithLabel(NodeLabels.User.name());
+				vertex.setProperty("nodeId", new AutoIncrementServiceImpl().getNextAutoIncrement());
 				vertex.setProperty("userId", userId);
 				
 				for(Entry<String, Object> userPropertiesEntry : userProperties.entrySet())

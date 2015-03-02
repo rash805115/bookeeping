f5ca2dacee9fcaea56289424eeaf6e075fe3987f
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-03-02 18:45:50 -0500

    Neo4JRestServiceImpl modularized
    This class is modularized into separate classes based on system
    entities.

---
+++ src/main/java/database/service/neo4jrest/impl/AutoIncrementServiceImpl.java
@@ -0,0 +1,49 @@
+package database.service.neo4jrest.impl;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.ReadableIndex;
+
+import database.NodeLabels;
+import database.connection.singleton.Neo4JRestConnection;
+import database.service.AutoIncrementService;
+
+public class AutoIncrementServiceImpl implements AutoIncrementService
+{
+	private GraphDatabaseService graphDatabaseService;
+	
+	public AutoIncrementServiceImpl()
+	{
+		this.graphDatabaseService = Neo4JRestConnection.getInstance().getGraphDatabaseServiceObject();
+	}
+	
+	@Override
+	public int getNextAutoIncrement()
+	{
+		Node autoIncrement = null;
+		try(Transaction transaction = this.graphDatabaseService.beginTx())
+		{
+			ReadableIndex<Node> readableIndex = this.graphDatabaseService.index().getNodeAutoIndexer().getAutoIndex();
+			autoIncrement = readableIndex.get("nodeId", 0).getSingle();
+			
+			if(autoIncrement == null)
+			{
+				Node node = this.graphDatabaseService.createNode(NodeLabels.AutoIncrement);
+				node.setProperty("nodeId", 0);
+				node.setProperty("next", 2);
+				
+				transaction.success();
+				return 1;
+			}
+			else
+			{
+				int nextAutoIncrement = (int) autoIncrement.getProperty("next");
+				autoIncrement.setProperty("next", nextAutoIncrement + 1);
+				
+				transaction.success();
+				return nextAutoIncrement;
+			}
+		}
+	}
+}
--- src/main/java/database/service/neo4jrest/impl/UserServiceImpl.java
+++ src/main/java/database/service/neo4jrest/impl/UserServiceImpl.java
@@ -1,63 +1,29 @@
-package database.service.impl;
+package database.service.neo4jrest.impl;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.index.ReadableIndex;
 
+import database.NodeLabels;
 import database.connection.singleton.Neo4JRestConnection;
-import database.service.DatabaseService;
+import database.service.UserService;
 import exception.DuplicateUser;
 import exception.UserNotFound;
 
-public class Neo4JRestServiceImpl implements DatabaseService
+public class UserServiceImpl implements UserService
 {
-	private enum Labels implements Label
-	{
-		AutoIncrement, User
-	}
-	
 	private GraphDatabaseService graphDatabaseService;
 	
-	public Neo4JRestServiceImpl()
+	public UserServiceImpl()
 	{
 		this.graphDatabaseService = Neo4JRestConnection.getInstance().getGraphDatabaseServiceObject();
 	}
-
-	@Override
-	public int getNextAutoIncrement()
-	{
-		Node autoIncrement = null;
-		try(Transaction transaction = this.graphDatabaseService.beginTx())
-		{
-			ReadableIndex<Node> readableIndex = this.graphDatabaseService.index().getNodeAutoIndexer().getAutoIndex();
-			autoIncrement = readableIndex.get("nodeId", 0).getSingle();
-			
-			if(autoIncrement == null)
-			{
-				Node node = this.graphDatabaseService.createNode(Labels.AutoIncrement);
-				node.setProperty("nodeId", 0);
-				node.setProperty("next", 2);
-				
-				transaction.success();
-				return 1;
-			}
-			else
-			{
-				int nextAutoIncrement = (int) autoIncrement.getProperty("next");
-				autoIncrement.setProperty("next", nextAutoIncrement + 1);
-				
-				transaction.success();
-				return nextAutoIncrement;
-			}
-		}
-	}
-
+	
 	@Override
 	public void createNewUser(String userId, Map<String, Object> userProperties) throws DuplicateUser
 	{
@@ -74,8 +40,8 @@ public class Neo4JRestServiceImpl implements DatabaseService
 			}
 			else
 			{
-				Node node = this.graphDatabaseService.createNode(Labels.User);
-				node.setProperty("nodeId", this.getNextAutoIncrement());
+				Node node = this.graphDatabaseService.createNode(NodeLabels.User);
+				node.setProperty("nodeId", new AutoIncrementServiceImpl().getNextAutoIncrement());
 				node.setProperty("userId", userId);
 				
 				for(Entry<String, Object> userPropertiesEntry : userProperties.entrySet())

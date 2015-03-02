6eebcb481c5b580fe1dd20cb98e4a8f68f322d2c
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-03-02 18:50:05 -0500

    Neo4JEmbeddedServiceImpltest modularized
    This test class is modularized into separate classes based on system
    entities.

---
+++ src/test/java/database/service/neo4jembedded/impl/AutoIncrementServiceTest.java
@@ -0,0 +1,34 @@
+package database.service.neo4jembedded.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import database.service.AutoIncrementService;
+
+public class AutoIncrementServiceTest
+{
+	private AutoIncrementService autoIncrementService;
+	
+	@Before
+	public void setup()
+	{
+		this.autoIncrementService = new AutoIncrementServiceImpl();
+	}
+	
+	@After
+	public void tearDown()
+	{
+		this.autoIncrementService = null;
+	}
+	
+	@Test
+	public void testGetNextAutoIncrement()
+	{
+		long nextAutoIncrement1 = this.autoIncrementService.getNextAutoIncrement();
+		long nextAutoIncrement2 = this.autoIncrementService.getNextAutoIncrement();
+		assertEquals(nextAutoIncrement1 + 1, nextAutoIncrement2);
+	}
+}

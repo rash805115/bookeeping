d9a1ebd923b824b4cea72a01c69997a9cfed6177
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2015-03-11 14:52:27 -0400

    Titan filesystem service impl
    Filesystem service implemented by titan cassandra.

---
+++ src/main/java/database/service/titancassandraembedded/impl/FilesystemServiceImpl.java
@@ -0,0 +1,175 @@
+package database.service.titancassandraembedded.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.thinkaurelius.titan.core.TitanGraph;
+import com.thinkaurelius.titan.core.TitanTransaction;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
+
+import database.connection.singleton.TitanCassandraEmbeddedConnection;
+import database.service.FilesystemService;
+import database.titan.NodeLabels;
+import database.titan.RelationshipLabels;
+import exception.DirectoryNotFound;
+import exception.DuplicateFilesystem;
+import exception.FileNotFound;
+import exception.FilesystemNotFound;
+import exception.UserNotFound;
+import exception.VersionNotFound;
+
+public class FilesystemServiceImpl implements FilesystemService
+{
+	private TitanGraph titanGraph;
+	
+	public FilesystemServiceImpl()
+	{
+		this.titanGraph = TitanCassandraEmbeddedConnection.getInstance().getTitanGraphObject();
+	}
+	
+	@Override
+	public void createNewFilesystem(String filesystemId, String userId, Map<String, Object> filesystemProperties) throws UserNotFound, DuplicateFilesystem
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			Vertex user = null;
+			try
+			{
+				CommonCode commonCode = new CommonCode();
+				user = commonCode.getUser(userId);
+				commonCode.getFilesystem(userId, filesystemId);
+				throw new DuplicateFilesystem("ERROR: Filesystem already present! - \"" + filesystemId + "\"");
+			}
+			catch(FilesystemNotFound filesystemNotFound)
+			{
+				AutoIncrementServiceImpl autoIncrementServiceImpl = new AutoIncrementServiceImpl();
+				
+				Vertex node = this.titanGraph.addVertexWithLabel(NodeLabels.Filesystem.name());
+				node.setProperty("nodeId", autoIncrementServiceImpl.getNextAutoIncrement());
+				node.setProperty("filesystemId", filesystemId);
+				node.setProperty("version", 0);
+				
+				for(Entry<String, Object> filesystemPropertiesEntry : filesystemProperties.entrySet())
+				{
+					node.setProperty(filesystemPropertiesEntry.getKey(), filesystemPropertiesEntry.getValue());
+				}
+				
+				Vertex rootDirectory = this.titanGraph.addVertexWithLabel(NodeLabels.Directory.name());
+				rootDirectory.setProperty("nodeId", autoIncrementServiceImpl.getNextAutoIncrement());
+				
+				user.addEdge(RelationshipLabels.has.name(), node);
+				node.addEdge(RelationshipLabels.has.name(), rootDirectory);
+				titanTransaction.commit();
+			}
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
+			}
+		}
+	}
+
+	@Override
+	public void createNewVersion(String userId, String filesystemId, Map<String, Object> changeMetadata, Map<String, Object> changedProperties) throws UserNotFound, FilesystemNotFound
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			CommonCode commonCode = new CommonCode();
+			Vertex filesystem = null;
+			try
+			{
+				filesystem = commonCode.getVersion("filesystem", userId, filesystemId, null, null, -1);
+			}
+			catch (VersionNotFound | FileNotFound | DirectoryNotFound e) {}
+			Vertex versionedFilesystem = commonCode.copyNodeTree(filesystem);
+			
+			int filesystemLatestVersion = (int) filesystem.getProperty("version");
+			versionedFilesystem.setProperty("version", filesystemLatestVersion + 1);
+			for(Entry<String, Object> entry : changedProperties.entrySet())
+			{
+				versionedFilesystem.setProperty(entry.getKey(), entry.getValue());
+			}
+			
+			Edge relationship = filesystem.addEdge(RelationshipLabels.hasVersion.name(), versionedFilesystem);
+			for(Entry<String, Object> entry : changeMetadata.entrySet())
+			{
+				relationship.setProperty(entry.getKey(), entry.getValue());
+			}
+			
+			titanTransaction.commit();
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
+			}
+		}
+	}
+
+	@Override
+	public void deleteFilesystemTemporarily(String userId, String filesystemId) throws UserNotFound, FilesystemNotFound
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			Vertex filesystem = new CommonCode().getFilesystem(userId, filesystemId);
+			Edge hasRelationship = filesystem.getEdges(Direction.IN, RelationshipLabels.has.name()).iterator().next();
+			Vertex parentDirectory = hasRelationship.getVertex(Direction.OUT);
+			
+			Edge hadRelationship = parentDirectory.addEdge(RelationshipLabels.had.name(), filesystem);
+			for(String key : hasRelationship.getPropertyKeys())
+			{
+				hadRelationship.setProperty(key, hasRelationship.getProperty(key));
+			}
+			
+			hasRelationship.remove();
+			titanTransaction.commit();
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
+			}
+		}
+	}
+
+	@Override
+	public Map<String, Object> getFilesystem(String userId, String filesystemId) throws UserNotFound, FilesystemNotFound
+	{
+		TitanTransaction titanTransaction = this.titanGraph.newTransaction();
+		
+		try
+		{
+			Vertex filesystem = new CommonCode().getFilesystem(userId, filesystemId);
+			Map<String, Object> filesystemProperties = new HashMap<String, Object>();
+			
+			Iterable<String> keys = filesystem.getPropertyKeys();
+			for(String key : keys)
+			{
+				filesystemProperties.put(key, filesystem.getProperty(key));
+			}
+			
+			titanTransaction.commit();
+			return filesystemProperties;
+		}
+		finally
+		{
+			if(titanTransaction.isOpen())
+			{
+				titanTransaction.rollback();
+			}
+		}
+	}
+}
